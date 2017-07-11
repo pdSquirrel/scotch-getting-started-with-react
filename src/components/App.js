230013fb477983c9470b0a8fa100938ffa781a09
Roger Nordqvist <progdev.squirrel@gmail.com>
2017-07-11 22:27:47 +0200

    changed searchbox to selection dropdown

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,7 @@ export default class App extends Component {
     super(props);
 
     this.state = {
+      weatherData: [],
       zipcode: '',
       city: {},
       dates: [],
@@ -19,13 +20,23 @@ export default class App extends Component {
     this.onDayClicked = this.onDayClicked.bind(this);
   }
 
-  onFormSubmit(zipcode) {
-    get(`http://localhost:3000/weather/${zipcode}`)
-    .then(({ data}) => {
-      const { city, list: dates } = data;
-
-      this.setState({ zipcode, city, dates, selectedDate: null });
+  componentDidMount() {
+    get(`http://localhost:3000/weather`)
+      .then(({ data: weatherData }) => {
+        this.setState({ weatherData });
     });
+  }
+
+  onFormSubmit(zipcode) {
+    const zip = zipcode * 1;
+    const { weatherData } = this.state;
+    const data = weatherData.find(w => w.id === zip);
+    const { city, list: dates} = data;
+
+    this.setState({ zip, city, dates, selectedDate: null });
+
+
+
   }
 
   onDayClicked(dayIndex) {
@@ -37,11 +48,12 @@ export default class App extends Component {
        pulling vars from state to get rid
        of 'this.state.' in the components bellow
     */
-    const { city, dates, selectedDate } = this.state;
+    const { weatherData, city, dates, selectedDate } = this.state;
+    const zips = weatherData.map(w => w.id);
 
     return(
       <div className="app">
-        <ZipForm onSubmit={this.onFormSubmit} />
+        <ZipForm zips={zips} onSubmit={this.onFormSubmit} />
         <WeatherList days={dates} onDayClicked={this.onDayClicked} />
         {
           /*
